9e7aaa7d5cd3a1773029a9159662c86de0824051
Ashraf Monshi <wh7r.com@gmail.com>
2022-09-24 16:46:31 +0300

    thunder assets and updates
    update filament to last version

--- tailwind-filament.config.js
+++ tailwind-filament.config.js
@@ -1,11 +1,46 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
     darkMode: 'class',
     content: [
-        '../wind-pkg/resources/views/filament/**/*.blade.php',
-        '../sky-pkg/resources/views/filament/**/*.blade.php',
-        '../sky-pkg/src/Models/PostStatus.php',
-        '../bolt-pkg/resources/views/filament/**/*.blade.php',
-        '../thunder-pkg/resources/views/filament/**/*.blade.php',
+        './resources/views/**/*.blade.php',
+        '../wind/resources/views/filament/**/*.blade.php',
+        '../sky/resources/views/filament/**/*.blade.php',
+        '../sky/src/Models/PostStatus.php',
+        '../bolt/resources/views/filament/**/*.blade.php',
+        '../bolt/src/filament/**/*.php',
+        '../thunder/resources/views/**/*.blade.php',
+        '../thunder/src/Models/TicketsStatus.php',
         './vendor/filament/**/*.blade.php',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                gray: colors.stone,
+                primary: colors.emerald,
+                green: colors.green,
+                secondary: colors.yellow,
+                danger: colors.rose,
+                success: colors.green,
+                warning: colors.yellow,
+                info: colors.blue,
+                emerald: {
+                    50: "#d1fae5",
+                    100: "#a7f3d0",
+                    200: "#6ee7b7",
+                    300: "#34d399",
+                    400: "#10b981",
+                    600: "#059669",
+                    500: "#059669",
+                    700: "#047857",
+                    800: "#065f46",
+                    900: "#064e3b",
+                },
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('tailwindcss-debug-screens'),
     ],
 };
