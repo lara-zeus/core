ccc6dc7fe5e26c8d164b01b8118ca1e9470bcf75
Ash Monsh <wh7r.com@gmail.com>
2023-06-16 19:38:13 +0300

    fix zeus core

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -3,6 +3,7 @@
 namespace LaraZeus\Core;
 
 use Filament\PluginServiceProvider;
+use Illuminate\Support\Facades\Blade;
 use Spatie\LaravelPackageTools\Package;
 
 class CoreServiceProvider extends PluginServiceProvider
@@ -17,9 +18,22 @@ class CoreServiceProvider extends PluginServiceProvider
         'zeus-filament' => __DIR__ . '/../resources/dist/filament.js',
     ];
 
+    public function boot(): CoreServiceProvider
+    {
+        // let me have my fun 🤷🏽‍
+        Blade::directive('zeus', function () {
+            /** @phpstan-ignore-next-line */
+            return '<span class="text-secondary-700 group"><span class="font-semibold text-primary-600 group-hover:text-secondary-500 transition ease-in-out duration-300">Lara&nbsp;<span class="line-through italic text-secondary-500 group-hover:text-primary-600 transition ease-in-out duration-300">Z</span>eus</span></span>';
+        });
+
+        return parent::boot();
+    }
+
     public function configurePackage(Package $package): void
     {
         parent::configurePackage($package);
-        $package->hasAssets();
+        $package
+            ->hasAssets()
+            ->hasViews();
     }
 }
