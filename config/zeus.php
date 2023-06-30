fee29613e0b4d7106041cdff60f4cab0c0ec2aaa
Ash Monsh <wh7r.com@gmail.com>
2023-06-30 04:26:12 +0300

    using the new theme system

--- config/zeus.php
+++ config/zeus.php
@@ -7,10 +7,12 @@ return [
      */
     'layout' => 'zeus::components.app',
     //'layout' => 'zeus::themes.breeze.layouts.app',
+    //'layout' => 'zeus::themes.daisyui.layouts.app',
 
     /**
      * the default theme, for now we only have one theme, and soon we will provide more free and premium themes.
      */
     'theme' => 'zeus',
     //'theme' => 'breeze',
+    //'theme' => 'daisyui',
 ];
