72930f8b8627039241ed05e121d5d3e3733becb1
Ash Monsh <wh7r.com@gmail.com>
2023-08-04 17:02:09 +0300

    add About Command for zeus version

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace LaraZeus\Core;
 
+use Composer\InstalledVersions;
 use Filament\Support\Assets\Css;
 use Filament\Support\Facades\FilamentAsset;
 use Illuminate\Support\Facades\App;
@@ -9,6 +10,7 @@ use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\View;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Foundation\Console\AboutCommand;
 
 class CoreServiceProvider extends PackageServiceProvider
 {
@@ -16,6 +18,23 @@ class CoreServiceProvider extends PackageServiceProvider
 
     public function packageBooted(): void
     {
+        if (class_exists(AboutCommand::class) && class_exists(InstalledVersions::class))
+        {
+            $packages = [
+                'Sky',
+                'Wind',
+                'Bolt',
+                'Thunder',
+                'Rain',
+                'Rhea',
+            ];
+            AboutCommand::add('Zeus', fn() => [
+                'Core Version' => InstalledVersions::getPrettyVersion('lara-zeus/core'),
+                'Packages' => collect($packages)
+                    ->filter(fn(string $package): bool => InstalledVersions::isInstalled("lara-zeus/{$package}"))
+                    ->join(', '),
+            ]);
+        }
         // let me have my fun 🤷🏽‍
         Blade::directive('zeus', function () {
             return '<span class="text-custom-700 group"><span class="font-semibold text-primary-600 group-hover:text-custom-500 transition ease-in-out duration-300">Lara&nbsp;<span class="line-through italic text-custom-500 group-hover:text-primary-600 transition ease-in-out duration-300">Z</span>eus</span></span>';
